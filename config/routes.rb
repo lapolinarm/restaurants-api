818b640b164c073ac4ac37124eb77d0b1c6d1687
Levi Apolinar <leviam51@gmail.com>
2024-09-25 09:47:04 -0500

    addesd routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :restaurants, only: [ :index ]
+    end
+  end
 end
