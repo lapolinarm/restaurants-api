df2abf15fa7cdf1042acd05abfcc715d63537f79
Levi Apolinar <leviam51@gmail.com>
2024-09-25 22:08:48 -0500

    added code to restaurants_controller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,7 @@
 # app/controllers/api/v1/restaurants_controller.rb
 class Api::V1::RestaurantsController < Api::V1::BaseController
-  before_action :set_restaurant, only: [ :show ]
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  before_action :set_restaurant, only: [ :show, :update ]
 
   def show
   end
@@ -10,10 +11,27 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
     render json: @restaurants
   end
 
+  def update
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
     authorize @restaurant  # For Pundit
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
