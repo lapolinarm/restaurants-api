cdf132cb1e922f5b55e32f999a954435bb18fbf5
Levi Apolinar <leviam51@gmail.com>
2024-09-25 21:22:32 -0500

    added controller for second endpoint, based on display

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,7 +1,19 @@
 # app/controllers/api/v1/restaurants_controller.rb
 class Api::V1::RestaurantsController < Api::V1::BaseController
+  before_action :set_restaurant, only: [ :show ]
+
+  def show
+  end
+
   def index
     @restaurants = policy_scope(Restaurant)
     render json: @restaurants
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant  # For Pundit
   end
 end
